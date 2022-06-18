457cf2f57677dd721d4816f30367fd4e0e68892f
Kishansharechat <kishangupta@sharechat.co>
2022-06-18 22:40:25 +0530

    question view in progress

--- shared/src/commonMain/kotlin/com/kk/eazypariksha/model/home/HomeList.kt
+++ shared/src/commonMain/kotlin/com/kk/eazypariksha/model/home/HomeList.kt
@@ -2,16 +2,17 @@ package com.kk.eazypariksha.model.home
 
 object HomeList {
     val list: List<HomeListModel> = listOf(
-        HomeListModel("Add Exam", "plus.square.fill.on.square.fill"),
-        HomeListModel("Drafts", "doc.fill.badge.ellipsis"),
-        HomeListModel("Upcoming Exams", "clock.arrow.2.circlepath"),
-        HomeListModel("result", "checkmark.seal.fill"),
-        HomeListModel("Search Students", "text.magnifyingglass")
+        HomeListModel("Add Exam", "plus.square.fill.on.square.fill", "AddExamViewController"),
+        HomeListModel("Drafts", "doc.fill.badge.ellipsis", "AddExamViewController"),
+        HomeListModel("Upcoming Exams", "clock.arrow.2.circlepath", "AddQuestionsViewController"),
+        HomeListModel("result", "checkmark.seal.fill","AddExamViewController"),
+        HomeListModel("Search Students", "text.magnifyingglass","AddExamViewController")
     )
 }
 
 
 data class HomeListModel(
     val title: String,
-    val image: String
+    val image: String,
+    val type: String
 )
