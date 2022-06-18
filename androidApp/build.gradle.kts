e9f6b12f62a5b2887fc3e715fec662900dfc3193
Kshitij Patil <kshitijpatil@sharechat.co>
2022-06-18 19:10:39 +0530

    android: Migrate to Compose only
    Signed-off-by: Kshitij Patil <kshitijpatil@sharechat.co>

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,34 @@ android {
             isMinifyEnabled = false
         }
     }
-}
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 
-dependencies {
-    implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    dependencies {
+        implementation(project(":shared"))
+        implementation("androidx.core:core-ktx:1.8.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+        implementation("com.google.android.material:material:1.6.1")
+
+        val composeVersion = "1.2.0-beta03"
+
+        implementation("androidx.activity:activity-compose:1.4.0")
+        implementation("androidx.compose.foundation:foundation-layout:$composeVersion")
+        implementation("androidx.compose.material:material:1.1.1")
+        implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+        implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+        implementation("androidx.compose.ui:ui-util:$composeVersion")
+        implementation("androidx.compose.runtime:runtime:$composeVersion")
+        debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    }
 }
--- androidApp/src/main/java/com/kk/eazypariksha/android/MainActivity.kt
+++ androidApp/src/main/java/com/kk/eazypariksha/android/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.kk.eazypariksha.android
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.kk.eazypariksha.Greeting
+import com.kk.samplecomposeapp.ui.theme.EazyParikshaTheme
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            EazyParikshaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Box(contentAlignment = Alignment.Center) {
+                        Text(text = "${Greeting().greeting()}!")
+                    }
+                }
+            }
+        }
     }
 }
